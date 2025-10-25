874e42cf38aa6886343c700ad2a1a89028e31d5e
HafsaHashmi123 <hafsahasmi123@gmail.com>
2025-10-25 03:31:20 -0700

    Update Account.java

--- Account.java
+++ Account.java
@@ -19,4 +19,36 @@ class Account {
         return password;
     }
 }
+import java.util.Scanner;
+
+public class EncapsulationLoginSystem {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Account user = new Account();
+
+        System.out.println("=== User Registration ===");
+        System.out.print("Create username: ");
+        user.setUsername(input.nextLine());
+
+        System.out.print("Create password: ");
+        user.setPassword(input.nextLine());
+
+        System.out.println("\n=== Login ===");
+        System.out.print("Enter username: ");
+        String enteredUser = input.nextLine();
+
+        System.out.print("Enter password: ");
+        String enteredPass = input.nextLine();
+
+        if (enteredUser.equals(user.getUsername()) && enteredPass.equals(user.getPassword())) {
+            System.out.println("Access granted. Welcome, " + user.getUsername() + "!");
+        } else {
+            System.out.println("Access denied. Please check your credentials.");
+        }
+
+        input.close();
+    }
+}
+
+
 
